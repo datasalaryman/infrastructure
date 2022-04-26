0a69fcc94eddadea8352ed32f8020be85286d3e7
Data Salaryman <datasalaryman@protonmail.com>
2022-04-26 09:33:15 +0800

    Create first instance

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    linode = {
+      source = "linode/linode"
+      version = "1.16.0"
+    }
+  }
+}
+
+variable "linode_token" {
+  description = "Linode personal access token"
+  sensitive = true
+}
+
+variable "root_pass" {
+  description = "Linode instance default root password"
+  sensitive = true
+}
+
+provider "linode" {
+    token = var.linode_token
+}
+
+resource "linode_instance" "test_instance" {
+    label = "test_instance"
+    image = "linode/ubuntu18.04"
+    region = "ap-south"
+    type = "g6-nanode-1"
+    root_pass = var.root_pass
+}
