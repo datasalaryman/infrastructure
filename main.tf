04399d9a0e73a2b12b0c7cf62373e67b4d4c3923
Data Salaryman <datasalaryman@protonmail.com>
2023-06-06 13:16:35 +0800

    fix: add key pairs to current infra

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "datasalaryman-tfstate"
+    key    = "prod/prod.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+  #  uncomment if on another machine with another default
+  #  AWS account
+  #  profile = "personal"
+}
+
+module "datasalaryman_domain" {
+  source = "./modules/low-level/route53-zone"
+  name = "datasalaryman.com"
+}
+
+module "master_key_pairs" {
+  source = "./modules/low-level/key-pair"
+  key_name_prefix = "master_key_"
+  public_keys_file = "./src/keys/master.txt"
+}
+
